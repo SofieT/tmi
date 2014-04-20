4c5d9e8304d8811772864142ac8b97ff12381517
warreee <schodtsward@gmail.com>
2014-04-20 16:34:33 +0200

    cirkels.txt

--- Main.py
+++ Main.py
@@ -5,11 +5,26 @@ def main():
 
 #Deze methode leest het input bestand en verwacht de naam: cirkles.txt
 
+
+
 def readInput():
+
     try:
         file = open('cirkels.txt')
     except:
         print("Het invoerbestand cirkels.txt werd niet gevonden!")
+    else:
+        algo = 0
+        aantalCirkels = 0
+        for line in file:
+            if algo == 0:
+                algo = line
+            elif aantalCirkels == 0:
+                aantalCirkels = line
+            else:
+                #maakt een nieuwe cirkel aan in de lijst
+                print("else")
+
 
 
 if __name__ == "__main__": main()
