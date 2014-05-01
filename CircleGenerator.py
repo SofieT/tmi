8066c5510fe15618aa36680ea5900540203ee4c1
Ward Schodts <wardschodts@hotmail.com>
2014-05-01 14:20:12 +0200

    output

--- CircleGenerator.py
+++ CircleGenerator.py
@@ -24,7 +24,7 @@ def write_to_file(cirkels):
             f.write(str(c.getYco()) + " ")
             f.write(str(c.getR()) + "\n")
 
-c = generate_cirkels(100)
+c = generate_cirkels(1000)
 write_to_file(c)
 
 
