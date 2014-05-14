5e1207f6f0310e768de3d0362b039571e6a3882a
Robin Goots <robin5210@ulyssis.org>
2014-05-14 10:15:23 +0200

    merge conflict

--- graphs.R
+++ graphs.R
@@ -3,9 +3,9 @@ algo2 = read.table("times2.txt")
 algo3 = read.table("times3.txt")
 
 x = seq(10, length(algo1$V1) * 10, 10)
-y = range(algo1)
+y = range(algo1$V1, algo2$V1, algo3$V1)
 
-plot(x, algo1$V1, type="n", ylab="Uitvoeringstijd (ms)", xlab="Aantal cirkels")
+plot(x, algo3$V1, type="n", ylab="Uitvoeringstijd (ms)", xlab="Aantal cirkels")
 colors <- rainbow(3)
 
 lines(x, algo1$V1, col=colors[1]) 
